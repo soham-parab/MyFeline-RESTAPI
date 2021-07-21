9a8e6b9e4e2eade40d4438dbf6433c70e0267fd1
soham-parab <57428835+soham-parab@users.noreply.github.com>
2021-07-21 15:00:28 +0530

    fixed validations

--- routes/LoginUser.js
+++ routes/LoginUser.js
@@ -6,34 +6,34 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
-   res.send("loginnnnnnn");
+  res.send("loginnnnnnn");
 });
 
 router.post("/", async (req, res) => {
-   const { error } = loginValidation(req.body);
-   if (error) return res.send(error.details[0].message).status(401);
+  const { error } = loginValidation(req.body);
+  if (error) return res.send(error.details[0].message).status(401);
 
-   //Email exists?
-   const userExists = await User.findOne({ email: req.body.email });
-   if (!userExists) {
-      return res.status(400).send("This email does not exist, please register");
-   }
+  //Email exists?
+  const userExists = await User.findOne({ email: req.body.email });
+  if (!userExists) {
+    return res.status(400).send("This email does not exist, please register.");
+  }
 
-   //Is password Correct?
-   const validPassword = await bcrypt.compare(
-      req.body.password,
-      userExists.password
-   );
+  //Is password Correct?
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    userExists.password
+  );
 
-   if (!validPassword) {
-      return res.status(400).send("Invalid Password");
-   }
+  if (!validPassword) {
+    return res.status(400).send("Invalid Password, please try again.");
+  }
 
-   const token = jwt.sign({ _id: userExists._id }, process.env.TOKEN_SECRET);
-   res.header("auth-token", token).json({
-      token: token,
-      userExists: userExists,
-   });
+  const token = jwt.sign({ _id: userExists._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).json({
+    token: token,
+    userExists: userExists,
+  });
 });
 
 module.exports = router;
